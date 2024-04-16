cb3e1255af719ade4051bea06c1de9bc42ebb663
DLeibner <prasina500@gmail.com>
2024-04-16 19:07:25 +0200

    add generic send and getfile methods to api request

--- src/main/engine/services/api-request.ts
+++ src/main/engine/services/api-request.ts
@@ -55,4 +55,11 @@ export class ApiRequest {
         return this.getRequest().delete(url, options);
     }
 
+    send(method: string, url: string, options: RequestOptions = {}) {
+        return this.getRequest().send(method, url, options);
+    }
+
+    getFile(url: string, type: string, options: RequestOptions = {}) {
+        return this.getRequest().getFile(url, type, options);
+    }
 }

995041fdd67a084d9481cd36d76edba9be008f5d
DLeibner <prasina500@gmail.com>
2024-06-04 12:24:52 +0200

    feat: add quality param to screenshot action

--- src/main/engine/actions/Page.screenshot.ts
+++ src/main/engine/actions/Page.screenshot.ts
@@ -1,9 +1,9 @@
 import { Action, params } from '../model/index.js';
 
 export class ScreenshotAction extends Action {
-    static $type = 'Page.screenshot';
-    static $icon = 'fas fa-camera';
-    static override $help = `
+    static readonly $type = 'Page.screenshot';
+    static readonly $icon = 'fas fa-camera';
+    static override readonly $help = `
 Captures the screenshot and sends it to the Job.
 
 ### Parameters
@@ -16,6 +16,8 @@ Captures the screenshot and sends it to the Job.
 - max width: maximum width for full page emulation
 - min height: minimum height for full page emulation
 - max height: maximum height for full page emulation
+- scroll to top: scroll the page to the top before taking a screenshot
+- quality: compression quality from range [0..100] (jpeg only)
 
 ### Use For
 
@@ -44,6 +46,9 @@ Captures the screenshot and sends it to the Job.
     @params.Boolean()
     scrollToTop: boolean = true;
 
+    @params.Number({ min: 0, max: 100 })
+    quality: number = 50;
+
     async exec() {
         if (this.scrollToTop) {
             await this.$page.evaluate(() => {
@@ -59,6 +64,7 @@ Captures the screenshot and sends it to the Job.
             maxHeight: this.maxHeight,
             minWidth: this.minWidth,
             maxWidth: this.maxWidth,
+            quality: this.quality,
         });
     }
 }
